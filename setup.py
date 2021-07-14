d640ab4d2e0aa396c30f0efe089c99ed72c32dd6
kintama48 <65843525+kintama48@users.noreply.github.com>
2021-07-14 20:30:21 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'array_utils',         
+  packages = ['array_utils'],   
+  version = '0.1',      
+  license='afl-3.0',        
+  description = 'Contains some common numpy array methods', 
+  author = 'Abdullah_Baig',                   
+  author_email = 'rebelschaos@gmail.com',      
+  url = 'https://github.com/kintama48/',   
+  download_url = 'https://github.com/kintama48/array_utils/archive/refs/tags/v_0.1.tar.gz',    
+  keywords = ['AMAX', 'AMIN', 'LINSPACE', 'WITHOUT NUMPY'],   
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: afl-3.0',  
+    'Programming Language :: Python :: 3.8.0+',
+  ],
+)
